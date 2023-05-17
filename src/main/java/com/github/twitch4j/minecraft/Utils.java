dd5dc30cff81e0072f0cf0044d3b0ef56146c923
Amir <50082485+erxson@users.noreply.github.com>
2023-05-17 18:58:26 +0300

    Refactoring after 2 years #3

--- src/main/java/com/github/twitch4j/minecraft/Utils.java
+++ src/main/java/com/github/twitch4j/minecraft/Utils.java
@@ -11,11 +11,11 @@ import org.bukkit.plugin.Plugin;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-
-import static java.util.logging.Level.INFO;
+import java.util.logging.Level;
 
 public class Utils {
     private static final Plugin plugin = TwitchMinecraftPlugin.getPlugin(TwitchMinecraftPlugin.class);
+
     public static void webhookSend() {
         try {
             String webhook = plugin.getConfig().getString("webhook_url");
@@ -23,27 +23,21 @@ public class Utils {
             String command = "curl -H \"Content-Type: application/json\" -X POST -d " + StringEscapeUtils.escapeJson(content.toString()) + " " + webhook;
             Process process = Runtime.getRuntime().exec(command);
             process.waitFor();
-            Bukkit.getLogger().log(INFO, "Content " + StringEscapeUtils.escapeJson(content.toString()));
-            Bukkit.getLogger().log(INFO, "Command " + command);
+            Bukkit.getLogger().log(Level.INFO, "Content: {0}\nCommand: {1}", new Object[] {StringEscapeUtils.escapeJson(content.toString()), command});
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
     }
-    public static int getNearMinute (int minutes, int value) {
-        int mod = minutes %  value;
-        int res = 0 ;
-        if ((mod) >= value % 2) {
-            res = minutes + (value - mod);
-        }else {
-            res = minutes - mod;
-        }
-        return res;
+
+    public static int getNearMinute(int minutes, int value) {
+        int mod = minutes % value;
+        return mod >= value / 2 ? minutes + (value - mod) : minutes - mod;
     }
+
     public static void download(String link, String file) {
-        URL asd = null;
         try {
-            asd = new URL(link);
-            FileUtils.copyURLToFile(asd, new File(file));
+            URL url = new URL(link);
+            FileUtils.copyURLToFile(url, new File(file));
         } catch (IOException e) {
             e.printStackTrace();
         }
