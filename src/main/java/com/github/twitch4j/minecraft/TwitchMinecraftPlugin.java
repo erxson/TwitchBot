de97353a5e90346b7f4626777b70f295e50191b5
Amir <50082485+erxson@users.noreply.github.com>
2023-05-17 18:56:30 +0300

    Refactoring after 2 years #2

--- src/main/java/com/github/twitch4j/minecraft/TwitchMinecraftPlugin.java
+++ src/main/java/com/github/twitch4j/minecraft/TwitchMinecraftPlugin.java
@@ -1,51 +1,62 @@
 package com.github.twitch4j.minecraft;
 
-import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
-import com.github.philippheuer.events4j.simple.SimpleEventHandler;
-import com.github.twitch4j.ITwitchClient;
-import com.github.twitch4j.TwitchClientBuilder;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
+import com.github.philippheuer.events4j.simple.SimpleEventHandler;
+import com.github.twitch4j.ITwitchClient;
+import com.github.twitch4j.TwitchClientBuilder;
 
 public class TwitchMinecraftPlugin extends JavaPlugin {
 
     private ITwitchClient client;
-    public static File webhookcontent;
+    private static File webhookcontent;
+
     @Override
     public void onEnable() {
-        this.saveDefaultConfig();
-        FileConfiguration config = getConfig();
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+
         webhookcontent = new File(getDataFolder(), "webhook.json");
-        if(!webhookcontent.exists()) {
+        if (!webhookcontent.exists()) {
             try {
                 webhookcontent.createNewFile();
-                try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                    new FileOutputStream(webhookcontent), StandardCharsets.UTF_8))) {
-                    writer.write("Create your message on https://discohook.org/, click \"JSON Data Editor\", copy everything and paste here.");
+                try (Writer writer = new BufferedWriter(
+                        new OutputStreamWriter(new FileOutputStream(webhookcontent), StandardCharsets.UTF_8))) {
+                    writer.write(
+                            "Create your message on https://discohook.org/, click \"JSON Data Editor\", copy everything and paste here.");
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        this.getCommand("tbreload").setExecutor(new TwitchCommandManager());
-        this.getCommand("tbwebhook").setExecutor(new TwitchCommandManager());
-        this.getCommand("tbupdate").setExecutor(new TwitchCommandManager());
 
+        getCommand("tbreload").setExecutor(new TwitchCommandManager());
+        getCommand("tbwebhook").setExecutor(new TwitchCommandManager());
+        getCommand("tbupdate").setExecutor(new TwitchCommandManager());
+
+        FileConfiguration config = getConfig();
         String token = config.getString("oauth_token");
         OAuth2Credential credential = StringUtils.isNotBlank(token) ? new OAuth2Credential("twitch", token) : null;
         client = TwitchClientBuilder.builder()
-            .withClientId(config.getString("client_id"))
-            .withClientSecret(config.getString("client_secret"))
-            .withEnableChat(true)
-            .withChatAccount(credential)
-            .withEnableHelix(true)
-            .withDefaultAuthToken(credential)
-            .build();
+                .withClientId(config.getString("client_id"))
+                .withClientSecret(config.getString("client_secret"))
+                .withEnableChat(true)
+                .withChatAccount(credential)
+                .withEnableHelix(true)
+                .withDefaultAuthToken(credential)
+                .build();
         List<String> channels = config.getStringList("channels");
         if (!channels.isEmpty()) {
             channels.forEach(name -> client.getChat().joinChannel(name));
@@ -55,6 +66,7 @@ public class TwitchMinecraftPlugin extends JavaPlugin {
         client.getEventManager().getEventHandler(SimpleEventHandler.class).registerListener(new TwitchEventHandler(this));
     }
 
+  
     @Override
     public void onDisable() {
         if (client != null) {
@@ -65,7 +77,6 @@ public class TwitchMinecraftPlugin extends JavaPlugin {
     }
 
     public ITwitchClient getTwitchClient() {
-        return this.client;
+        return client;
     }
-
 }
