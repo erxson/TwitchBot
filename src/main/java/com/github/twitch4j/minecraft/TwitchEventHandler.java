fd2d8842ac8f9d282f800eaf0392ac71e3bb9fa2
Amir <50082485+erxson@users.noreply.github.com>
2023-05-20 18:53:24 +0300

    Update TwitchEventHandler.java

--- src/main/java/com/github/twitch4j/minecraft/TwitchEventHandler.java
+++ src/main/java/com/github/twitch4j/minecraft/TwitchEventHandler.java
@@ -20,11 +20,10 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 import static com.github.twitch4j.minecraft.Utils.*;
-import static java.util.logging.Level.SEVERE;
 
 
 public class TwitchEventHandler {
-    
+
     private final TwitchMinecraftPlugin plugin;
     private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
@@ -35,10 +34,16 @@ public class TwitchEventHandler {
     @EventSubscriber
     public void onStreamUp(ChannelGoLiveEvent event) {
         Stream stream = event.getStream();
+        String stream_up = plugin.getConfig().getString("stream_up");
 
         // Broadcast stream up message
-        broadcast(String.format(plugin.getConfig().getString("stream_up"), stream.getUserName(), stream.getTitle())); 
-        
+        // broadcast(String.format(plugin.getConfig().getString("stream_up"), stream.getUserName(), stream.getTitle()));
+        broadcast(stream_up
+                .replace("{streamer}", stream.getUserName())
+                .replace("{game}", stream.getGameName())
+                .replace("{title}", stream.getTitle())
+        );
+
         // Send Discord notification if the integration is enabled
         if (Boolean.parseBoolean(plugin.getConfig().getString("discord"))) {
             String webhookUrl = plugin.getConfig().getString("webhook_url");
@@ -53,18 +58,25 @@ public class TwitchEventHandler {
         if (Boolean.parseBoolean(plugin.getConfig().getString("duration"))) {
             int delay = plugin.getConfig().getInt("message_delay");
             int roundTo = plugin.getConfig().getInt("round_to");
-            
+
             executor.scheduleAtFixedRate(() -> {
                 if (stream.getType().equals("live")) {
                     int timeDiff = Instant.now().atZone(ZoneOffset.UTC).getMinute() - stream.getStartedAtInstant().atZone(ZoneOffset.UTC).getMinute();
                     int durationInMinutes = getNearMinute(timeDiff, roundTo);
                     String durationString = DurationFormatUtils.formatDuration(
                         TimeUnit.MINUTES.toMillis(durationInMinutes),
-                        "HH' hours and 'mm' minutes'", 
+                        "HH' hours and 'mm' minutes'",
                         false
                     );
                     // Broadcast stream duration message
-                    broadcast(String.format(plugin.getConfig().getString("stream_duration"), durationString, stream.getUserName()));
+                    // broadcast(String.format(plugin.getConfig().getString("stream_duration"), durationString, stream.getUserName()));
+
+                    broadcast(stream_up
+                            .replace("{streamer}", stream.getUserName())
+                            .replace("{game}", stream.getGameName())
+                            .replace("{title}", stream.getTitle())
+                            .replace("{duration}", durationString)
+                    );
                 }
             }, delay, delay, TimeUnit.MILLISECONDS);
         }
@@ -72,9 +84,14 @@ public class TwitchEventHandler {
 
     @EventSubscriber
     public void onStreamDown(ChannelGoOfflineEvent event) {
+        String stream_down = plugin.getConfig().getString("stream_down");
+
         // Broadcast stream down message
-        broadcast(String.format(plugin.getConfig().getString("stream_down"), event.getChannel().getName()));
-        
+        // broadcast(String.format(plugin.getConfig().getString("stream_down"), event.getChannel().getName()));
+        broadcast(stream_down
+                .replace("{streamer}", event.getChannel().getName())
+        );
+
         // Shutdown the stream duration message executor
         if (Boolean.parseBoolean(plugin.getConfig().getString("duration"))) {
             executor.shutdown();
@@ -89,39 +106,85 @@ public class TwitchEventHandler {
 
     @EventSubscriber
     public void onFollow(FollowEvent event) {
+        String on_follow = plugin.getConfig().getString("on_follow");
+
         // Broadcast follow message if the feature is enabled
         if (Boolean.parseBoolean(plugin.getConfig().getString("follow"))) {
-            broadcast(String.format(plugin.getConfig().getString("on_follow"), event.getUser().getName()));
+            // broadcast(String.format(plugin.getConfig().getString("on_follow"), event.getUser().getName()));
+            broadcast(on_follow
+                    .replace("{user}", event.getUser().getName())
+                    .replace("{streamer}", event.getChannel().getName())
+            );
         }
     }
 
     @EventSubscriber
     public void onCheer(CheerEvent event) {
+        String on_cheer = plugin.getConfig().getString("on_cheer");
+
         // Broadcast cheer message if the feature is enabled and the cheer amount is greater than or equal to 100
         int cheerThreshold = 100;
         if (Boolean.parseBoolean(plugin.getConfig().getString("cheer")) && event.getBits() >= cheerThreshold) {
-            broadcast(String.format(plugin.getConfig().getString("on_cheer"), event.getUser().getName(), event.getBits()));
+            // broadcast(String.format(plugin.getConfig().getString("on_cheer"), event.getUser().getName(), event.getBits()));
+            broadcast(on_cheer
+                    .replace("{streamer}", event.getChannel().getName())
+                    .replace("{user}", event.getUser().getName())
+                    .replace("{bits}", String.valueOf(event.getBits()))
+                    .replace("{message}", event.getMessage())
+                    .replace("{sub_months}", String.valueOf(event.getSubscriberMonths()))
+                    .replace("{sub_tier}", String.valueOf(event.getSubscriptionTier()))
+            );
         }
     }
 
     @EventSubscriber
     public void onSub(SubscriptionEvent event) {
+        String on_sub = plugin.getConfig().getString("on_sub");
+        String on_sub_gift = plugin.getConfig().getString("on_sub_gift");
+
         // Broadcast subscription message if the feature is enabled and the subscription is not a gift
-        if (Boolean.parseBoolean(plugin.getConfig().getString("sub")) && !event.getGifted()) {
-            broadcast(String.format(plugin.getConfig().getString("on_sub"), event.getUser().getName(), event.getMonths()));
+        if (Boolean.parseBoolean(plugin.getConfig().getString("sub"))) {
+            if (!event.getGifted()) {
+                // broadcast(String.format(plugin.getConfig().getString("on_sub"), event.getUser().getName(), event.getMonths()));
+                broadcast(on_sub
+                        .replace("{streamer}", event.getChannel().getName())
+                        .replace("{user}", event.getUser().getName())
+                        .replace("{sub_plan}", event.getSubscriptionPlan())
+                        .replace("{months}", event.getMonths().toString())
+                        .replace("{sub_streak}", event.getSubStreak().toString())
+                );
+            } else {
+                broadcast(on_sub_gift
+                        .replace("{streamer}", event.getChannel().getName())
+                        .replace("{user}", event.getGiftedBy().getName())
+                        .replace("{gifted}", event.getUser().getName())
+                        .replace("{sub_plan}", event.getSubscriptionPlan())
+                        .replace("{gift_months}", event.getGiftMonths().toString())
+                        .replace("{months}", event.getMonths().toString())
+                        .replace("{sub_streak}", event.getSubStreak().toString())
+                );
+            }
         }
     }
 
     @EventSubscriber
     public void onSubMysteryGift(GiftSubscriptionsEvent event) {
+        String on_sub_gift = plugin.getConfig().getString("on_sub_mystery");
+
         // Broadcast subscription gift message if the feature is enabled
-        if (Boolean.parseBoolean(plugin.getConfig().getString("sub_gift"))) {
-            broadcast(String.format(plugin.getConfig().getString("on_sub_gift"), event.getUser().getName(), event.getCount(), event.getChannel().getName()));
+        if (Boolean.parseBoolean(plugin.getConfig().getString("sub_mystery"))) {
+            // broadcast(String.format(plugin.getConfig().getString("on_sub_gift"), event.getUser().getName(), event.getCount(), event.getChannel().getName()));
+            broadcast(on_sub_gift
+                    .replace("{streamer}", event.getChannel().getName())
+                    .replace("{user}", event.getUser().getName())
+                    .replace("{sub_plan}", event.getSubscriptionPlan())
+                    .replace("{subs_gifted}", String.valueOf(event.getCount()))
+                    .replace("{subs_gifted_total}", String.valueOf(event.getTotalCount()))
+            );
         }
     }
 
     private void broadcast(String message) {
         Bukkit.broadcastMessage(Objects.requireNonNull(message, "message must not be null"));
     }
-
 }
